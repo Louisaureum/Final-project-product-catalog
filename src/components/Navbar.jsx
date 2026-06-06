5e883c0d8de0f924b355ddbe35aa823ba41c9d7e
Developer <developer@example.com>
2026-06-06 03:54:03 +0300

    Navbar: add links list and home link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,3 +2,5 @@ function Navbar({ title = 'Product Catalog' }) {
   return (
     <nav className="navbar">
       <div className="navbar-brand">{title}</div>
+      <ul className="navbar-links">
+        <li><a href="/">Home</a></li>
